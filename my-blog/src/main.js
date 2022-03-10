fc0d629f358fb639898afdf0c61688cd6b04e342
Aiden8641 <aiden18288@gmail.com>
2022-03-10 13:13:15 -0500

    firebaase stuff

--- my-blog/src/main.js
+++ my-blog/src/main.js
@@ -5,4 +5,6 @@ import router from './router'
 // global styles
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+//import store 
+import store from './store/index.js'
+createApp(App).use(router).use(store).mount('#app')
