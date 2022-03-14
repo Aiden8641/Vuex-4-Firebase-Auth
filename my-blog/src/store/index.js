1c0e92ced36756c7774746eaaa732455a2e7d183
Aiden8641 <aiden18288@gmail.com>
2022-03-14 12:56:51 -0400

    maybe

--- my-blog/src/store/index.js
+++ my-blog/src/store/index.js
@@ -1,5 +1,11 @@
 import {createStore} from "vuex"
 
+
+//firebase import store
+import {auth } from '../firebase/config'
+import {
+    createUserWithEmailAndPassword
+} from 'firebase/auth'
 const store = createStore({
     state: {
         /* points: 0 */
@@ -15,13 +21,19 @@ const store = createStore({
         }
     },
     actions: {
-        signup(context, {email, password}) {
+        async signup(context, {email, password}) {
             console.log('signup action')
 
             //async code 
-            setTimeout(() => {
+           /*  setTimeout(() => {
                 context.commit('setUser', { email, password })
-            }, 2000)
+            }, 2000) */
+            const res = await createUserWithEmailAndPassword(auth, email, password)
+            if (res) {
+                context.commit('setUser', res.user)
+            } else {
+                throw new Error('could not complete signup')
+            }
         }
     }
 })
