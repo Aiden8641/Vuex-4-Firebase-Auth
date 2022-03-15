40aef0831bba2aa246c66c6633895d1c07dba0be
Aiden8641 <aiden18288@gmail.com>
2022-03-15 13:04:30 -0400

    hkhkjlkjh

--- my-blog/src/store/index.js
+++ my-blog/src/store/index.js
@@ -1,42 +1,69 @@
-import { createStore } from "vuex"
-
+import { createStore } from "vuex";
 
 //firebase import store
-import { auth } from '../firebase/config'
+import { auth } from "../firebase/config";
 import {
-    createUserWithEmailAndPassword
-} from 'firebase/auth'
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 const store = createStore({
-    state: {
-        /* points: 0 */
-        user: null
-    },
-    mutations: {
-        /* updatePoints(state, payload){
+  state: {
+    /* points: 0 */
+    user: null,
+    authIsReady: false,
+  },
+  mutations: {
+    /* updatePoints(state, payload){
             state.points =  state.points + payload
         } */
-        setUser(state, payload) {
-            state.user = payload
-            console.log('user state changed:', state.user)
-        }
+    setUser(state, payload) {
+      state.user = payload;
+      console.log("user state changed:", state.user);
     },
-    actions: {
-        async signup(context, {email, password}) {
-            console.log('signup action')
+    setAuthIsReady(state, payload) {
+      state.authIsReady = payload;
+    },
+  },
+  actions: {
+    async signup(context, { email, password }) {
+      console.log("signup action");
 
-            //async code 
-           /*  setTimeout(() => {
+      //async code
+      /*  setTimeout(() => {
                 context.commit('setUser', { email, password })
             }, 2000) */
-            const res = await createUserWithEmailAndPassword(auth, email, password)
-            if (res) {
-                context.commit('setUser', res.user)
-            } else {
-                throw new Error('could not complete signup')
-            }
-        }
-    }
-})
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      if (res) {
+        context.commit("setUser", res.user);
+      } else {
+        throw new Error("could not complete signup");
+      }
+    },
+    async login(context, { email, password }) {
+      console.log("login action");
 
-//export store 
-export default store 
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      if (res) {
+        context.commit("setUser", res.user);
+      } else {
+        throw new Error("could not complete login");
+      }
+    },
+    async logout(context) {
+      console.log("logout action");
+
+      await signOut(auth);
+      context.commit("setUser", null);
+    },
+  },
+});
+
+const unsub = onAuthStateChanged(auth, (user) => {
+  store.commit("setAuthIsReady", true);
+  store.commit("setUser", user);
+  unsub();
+});
+//export store
+export default store;
--- my-blog/src/views/Login.vue
+++ my-blog/src/views/Login.vue
@@ -3,28 +3,52 @@
     <h3>Login</h3>
 
     <label for="email">Email:</label>
-    <input type="email" name="email" v-model="email" required>
+    <input type="email" name="email" v-model="email" required />
 
     <label for="email">Password:</label>
-    <input type="password" name="password" v-model="password" required>
+    <input type="password" name="password" v-model="password" required />
 
     <button>Login</button>
+    <div v-if="error">{{ error }}</div>
   </form>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
 export default {
   setup() {
-    const email = ref('')
-    const password = ref('')
+    const email = ref("");
+    const password = ref("");
 
-    const handleSubmit = () => {
+    /* const handleSubmit = () => {
       console.log(email.value, password.value)
-    }
+    } */
 
-    return { handleSubmit, email, password }
-  }
-}
+    const error = ref(null);
+
+    const store = useStore();
+    const router = useRouter();
+
+    const handleSubmit = async () => {
+      try {
+        await store.dispatch("login", {
+          email: email.value,
+          password: password.value,
+        });
+        router.push("/");
+      } catch (err) {
+        error.value = err.message;
+      }
+      /* store.dispatch("signup", {
+        email: email.value,
+        password: password.value,
+      }); */
+    };
+
+    return { handleSubmit, email, password, error };
+  },
+};
 </script>
